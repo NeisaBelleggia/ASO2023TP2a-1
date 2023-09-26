338d7487e9477c9c9f2e432e47ba76a795f1a07c
NeisaBelleggia <belleggian@gmail.com>
2023-09-26 12:46:39 -0300

    Hamburgesas corregidas, by Neisa!

--- bigsix-con-hilos.c
+++ bigsix-con-hilos.c
@@ -1,44 +1,49 @@
-#include <pthread.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdatomic.h>
+
 #define NUMBER_OF_THREADS 2
 #define CANTIDAD_INICIAL_HAMBURGUESAS 20
-int cantidad_restante_hamburguesas = CANTIDAD_INICIAL_HAMBURGUESAS; 
 
+int cantidad_restante_hamburguesas = CANTIDAD_INICIAL_HAMBURGUESAS;
+atomic_int turn = ATOMIC_VAR_INIT(0);
 
-void *comer_hamburguesa(void *tid) { 
-while(1 == 1){ //repetir infinitamente hasta que fuerce la terminacion del hilo con pthread_exit
-	
-	if (cantidad_restante_hamburguesas > 0) {
-	printf("Hola! soy el hilo(comensal) %d , me voy a comer una hamburguesa ! ya que todavia queda/n %d \n",tid, cantidad_restante_hamburguesas);
-	cantidad_restante_hamburguesas--; //me como una hamburguesa
-	} else
-	{
-	printf("SE TERMINARON LAS HAMBURGUESAS :( \n"); 
-	pthread_exit(NULL); //forzar terminacion del hilo
-	}
-
-}
-
-}
-
-int main(int argc, char *argv[]) { 
-	pthread_t threads[NUMBER_OF_THREADS]; 
-	int status,i,ret ; 
-	for(int i=0; i < NUMBER_OF_THREADS; i++) { 
-		printf("Hola!, soy el hilo principal. Estoy creando el hilo %d \n", i); 
-		status = pthread_create(&threads[i], NULL, comer_hamburguesa, (void *)i); 
-		if (status != 0) { 
-		printf("Algo salio mal, al crear el hilo recibi el codigo de error %d \n", status);
-		exit(-1); 
-		} 
-	} 
-
-	for (i= 0; i < NUMBER_OF_THREADS; i++) {
-        void *retval;
-        ret = pthread_join(threads[i], &retval); //espero por la terminacion de los hilos que cree
+void *comer_hamburguesa(void *tid) {
+    while (1) {
+        while (atomic_load(&turn) != (long)tid) {
+            // Espera activa hasta que sea el turno de este hilo
         }
-        pthread_exit(NULL); //como los hilos que cree ya terminaron de ejecutarse, termino yo tambien.
 
-} 
+        if (cantidad_restante_hamburguesas > 0) {
+            printf("Hola! Soy el hilo (comensal) %ld, me voy a comer una hamburguesa! Quedan %d\n", (long)tid, cantidad_restante_hamburguesas);
+            cantidad_restante_hamburguesas--;
+        } else {
+            printf("SE TERMINARON LAS HAMBURGUESAS %ld :(\n", (long)tid);
+            pthread_exit(NULL);
+        }
 
+        atomic_store(&turn, ((long)tid + 1) % NUMBER_OF_THREADS); // Cambiar el turno al siguiente hilo
+    }
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t threads[NUMBER_OF_THREADS];
+    int status, i, ret;
+
+    for (i = 0; i < NUMBER_OF_THREADS; i++) {
+        printf("Hola!, soy el hilo principal. Estoy creando el hilo %d\n", i);
+        status = pthread_create(&threads[i], NULL, comer_hamburguesa, (void *)(long)i);
+        if (status != 0) {
+            printf("Algo salió mal, al crear el hilo recibi el código de error %d\n", status);
+            exit(-1);
+        }
+    }
+
+    for (i = 0; i < NUMBER_OF_THREADS; i++) {
+        void *retval;
+        ret = pthread_join(threads[i], &retval);
+    }
+
+    pthread_exit(NULL);
+}
